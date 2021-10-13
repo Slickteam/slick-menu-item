77ed3c795ab525e10567f5d63d6d185c2023cecb
tlevisage <theo.levisage@slickteam.fr>
2021-10-13 15:31:15 +0200

    first commit

---
+++ src/my-element.ts
@@ -0,0 +1,89 @@
+import { html, css, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('slick-menu-item')
+export class SlickMenuItem extends LitElement {
+  static styles = css`
+    button {
+      display: flex;
+      border: none;
+      background: inherit;
+      color: currentcolor;
+      justify-content: center;
+      align-items: center;
+      height: 35px;
+    }
+    
+    button > * {
+      padding: 5px;
+      font-size: 18px;
+    }
+  `
+
+  /**
+   * The name to say "Hello" to.
+   */
+  @property()
+  title = 'Title'
+
+  /**
+   * The name to say "Hello" to.
+   */
+  @property()
+  icon = 'M2.5,19H21.5V21H2.5V19M22.07,9.64C21.86,8.84 21.03,8.36 20.23,8.58L14.92,10L8,3.57L6.09,4.08L10.23,11.25L5.26,12.58L3.29,11.04L1.84,11.43L3.66,14.59L4.43,15.92L6.03,15.5L11.34,14.07L15.69,12.91L21,11.5C21.81,11.26 22.28,10.44 22.07,9.64Z'
+
+  /**
+   * Returns a true or false value to display or hide the side menu
+   */
+  @property()
+  color = '#ffffff'
+
+  /**
+   * Returns a true or false value to display or hide the side menu
+   */
+  @property()
+  background = '#f97316'
+
+  colorInversion(e: any) {
+    e.currentTarget.setAttribute('style', `background: ${this.color}; color: ${this.background};`)
+  }
+
+  backToNormal(e: any) {
+    e.currentTarget.setAttribute('style', `background: ${this.background}; color: ${this.color};`)
+  }
+
+  emitEvent() {
+    console.log('slick-menu-item-clicked emitted !!')
+    dispatchEvent(new Event('slick-menu-item-clicked'));
+  }
+
+  render() {
+    return html`
+      <li
+          style="color: ${this.color};"
+          @mouseover="${this.colorInversion}"
+          @mouseout="${this.backToNormal}"
+      >
+        <button
+            @click="${this.emitEvent}"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="${ this.icon }"></path></svg>
+          <span class="link_name">${ this.title }</span>
+        </button>
+      </li>
+    `
+  }
+
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'slick-menu-item': SlickMenuItem
+  }
+}
